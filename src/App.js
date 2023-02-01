2c1b04b95a38bd575cbd4b8f8791b9cede687afe
Scott L <ehee27@gmail.com>
2023-02-01 08:24:10 -0600

    morning

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import Header from './Header';
+import Nav from './Nav';
+import Footer from './Footer';
+import Home from './Home';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+import About from './About';
+import Missing from './Missing';
+import { Route, Switch, useHistory } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { format } from 'date-fns';
+
+const App = () => {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: 'My first post',
+      datetime: 'July 01, 2021 11:17:36 AM',
+      body: 'This is the body of the dummy post.',
+    },
+    {
+      id: 2,
+      title: 'My Second post',
+      datetime: 'July 01, 2021 11:17:36 AM',
+      body: 'This is the body of the dummy post.',
+    },
+    {
+      id: 3,
+      title: 'My Third post',
+      datetime: 'July 01, 2021 11:17:36 AM',
+      body: 'This is the body of the dummy post.',
+    },
+    {
+      id: 4,
+      title: 'My Forth post',
+      datetime: 'July 01, 2021 11:17:36 AM',
+      body: 'This is the body of the dummy post.',
+    },
+  ]);
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [postTitle, setPostTitle] = useState('');
+  const [postBody, setPostBody] = useState('');
+
+  const history = useHistory();
+  //
+
+  useEffect(() => {
+    const filteredResults = posts.filter(
+      post =>
+        post.body.toLowerCase().includes(search.toLowerCase()) ||
+        post.title.toLowerCase().includes(search.toLowerCase())
+    );
+    setSearchResults(filteredResults.reverse());
+  }, [posts, search]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+    const newPost = { id, title: postTitle, datetime, body: postBody };
+    const allPosts = [...posts, newPost];
+    setPosts(allPosts);
+    setPostTitle('');
+    setPostBody('');
+    history.push('/');
+  };
+  //
+  const handleDelete = id => {
+    const postList = posts.filter(post => post.id !== id);
+    setPosts(postList);
+    history.push('/');
+  };
+  //
+  return (
+    <div className="App">
+      <Header title="React JS Blog" />
+      <Nav search={search} setSearch={setSearch} />
+      <Switch>
+        <Route exact path="/">
+          <Home posts={searchResults} />
+        </Route>
+        <Route exact path="/post">
+          <NewPost
+            handleSubmit={handleSubmit}
+            postTitle={postTitle}
+            setPostTitle={setPostTitle}
+            postBody={postBody}
+            setPostBody={setPostBody}
+          />
+        </Route>
+        <Route path="/post/:id">
+          <PostPage posts={posts} handleDelete={handleDelete} />
+        </Route>
+        <Route path="/about" component={About} />
+        <Route path="*" component={Missing} />
+      </Switch>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
